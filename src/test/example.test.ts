d140a61df5085d2c0c7c1ca468c5b972a48d8f60
Ronan Klyne <ronan.klyne@cazoo.co.uk>
2021-02-19 14:51:49 +0000

    --wip-- [skip ci]

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,15 +1,22 @@
+type Direction = "N" | "S" | "E" | "W";
+type RoverPosition = [number, number, Direction];
 
 class Rover {
-    getPosition() {
-        return ''
-    }
+  position: RoverPosition;
+
+  constructor() {
+    this.position = [0, 0, "N"];
+  }
+  getPosition() {
+    return this.position.join(":");
+  }
 }
 
-describe('mars rover', () => {
-    it("the rover has an initial position on the plateau", () => {
-        const rover = new Rover()
-        const initialPosition = rover.getPosition()
+describe("mars rover", () => {
+  it("the rover has an initial position on the plateau", () => {
+    const rover = new Rover();
+    const initialPosition = rover.getPosition();
 
-        expect(initialPosition).toBe('0:0:N')
-    })
-})
+    expect(initialPosition).toBe("0:0:N");
+  });
+});
